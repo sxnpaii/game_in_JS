4e9c40634fce3431e09caf4cd8bf8fbc96e7b3d2
Khurshid <axurshidbek2005@gmail.com>
2022-09-15 17:10:20 +0500

    ddwfedfd

---
+++ script.js
@@ -0,0 +1,55 @@
+let blueCar = document.getElementById('bluecar')
+let raceCar = document.getElementById('racecar')
+let result = document.getElementById('result')
+const score = document.getElementById('score')
+var game = document.getElementById('game')
+
+var counter = 0;
+var jumpsound = document.getElementById('jumpsound');
+
+blueCar.addEventListener('animationiteration', () => {
+    var random = (Math.floor(Math.random() * 5) * 65)
+    blueCar.style.left = random + 'px';
+    counter++;
+});
+
+
+
+window.addEventListener("keydown", function(e) {
+    if(e.keyCode == "39"){
+        var raceCarLeft = parseInt(window.getComputedStyle(raceCar).getPropertyValue("left"));
+
+        if(raceCarLeft < 260){
+            raceCar.style.left = (raceCarLeft + 65) + 'px';
+
+        }
+        jumpsound.play();
+    };
+
+    if(e.keyCode == "37"){
+        var raceCarLeft = parseInt(window.getComputedStyle(raceCar).getPropertyValue("left"));
+        if(raceCarLeft > 0 ){
+            raceCar.style.left = (raceCarLeft - 65) + 'px'
+            jumpsound.play();
+        }
+    }
+});
+
+
+
+
+
+setInterval(function GameOver() {
+    var blueCarTop = parseInt(window.getComputedStyle(blueCar).getPropertyValue('top'));
+    var blueCarLeft = parseInt(window.getComputedStyle(blueCar).getPropertyValue('left'));
+    var raceCarLeft = parseInt(window.getComputedStyle(raceCar).getPropertyValue('left'));
+    if (blueCarLeft === raceCarLeft && blueCarTop > 250 && blueCarTop > 450) {
+        result.style.display = 'block';
+        game.style.display = 'none';
+        score.innerHTML =`score: ${counter}`;
+        counter = 0;
+    }
+
+});
+
+
